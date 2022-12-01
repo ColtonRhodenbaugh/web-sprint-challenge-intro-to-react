f462a179c807214d4802489ae048cde8899e2c21
Colton Rhodenbaugh <coltonrhodenbaugh@Coltons-Air.hsd1.il.comcast.net>
2022-12-01 16:37:32 -0600

    Still working

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+import React from "react";
+import StyledCharacters from "./StyledChar"
+
+const Characters = (props) => {
+    const { data } = props;
+
+    return <div className="organize-sec">
+        {data.map((listOfCharacters) => {
+            return(
+                <>
+                    <StyledCharacters>
+                        <h1 className="StarNames">{listOfCharacters.name}</h1>
+                        <h2>Gender: {listOfCharacters.gender}</h2>
+                        <h2>Height: {listOfCharacters.height}</h2>
+                        <h2>Mass: {listOfCharacters.mass}</h2>
+                        <h2>Birth Year: {listOfCharacters.birth_year}</h2>
+                        <h2>Hair Color: {listOfCharacters.hair_color}</h2>
+                        <h2>Eye Color: {listOfCharacters.eye_color}</h2>
+                    </StyledCharacters>
+                </>
+            )
+        })}
+    </div>
+}
+export default Characters
---
+++ src/components/StyledChar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import  styledComponent  from "styled-components";
+import styled from 'styled-components'
+
+const StyledCharacters = styled.div`
+.StarNames{
+    :hover{
+        color: cyan;
+    }
+}
+`
+export default StyledCharacters
